cf4e61397a720360ed35d15053aba7c3d6f8be38
youssefelgringo <youssefcoco800@gmail.com>
2022-11-06 11:42:34 +0100

    j'essaie le js mais bon...

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
 let myHeading = document.querySelector("h1");
+
+window.onload=function(){
+    var bouton = document.getElementById('divForm');
+    var nav = document.getElementById('nav');
+    bouton.onclick = function(e){
+        if(nav.style.display=="block"){
+            nav.style.display="none";
+        }else{
+            nav.style.display="block";
+        }
+    };
+};
